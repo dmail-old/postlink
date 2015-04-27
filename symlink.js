03aaf7c8fa1cad0bbca791242ad8d7194783493c
Damien Maillard <dmaillard06@gmail.com>
2015-04-27 13:04:06 +0200

    recurse source folder + link duplicate

--- symlink.js
+++ symlink.js
@@ -9,16 +9,33 @@ var fs = require('fs');
 var childProcess = require('child_process');
 var path = require('path');
 
-function collectModules(filename){
-	var modules = [];
+function collectModulePaths(filename){
+	var modulePaths = [];
 
-	return fs.readdirSync(filename).map(function(name){
-		return filename + path.sep + name;
-	}).filter(function(filepath){
-		return fs.statSync(filepath).isDirectory() && fs.existsSync(filepath + path.sep + 'package.json');
-	}).map(function(modulePath){
-		var meta = JSON.parse(fs.readFileSync(modulePath + path.sep + 'package.json'));
-		
+	fs.readdirSync(filename).map(function(name){
+		return path.join(filename, name);
+	}).forEach(function(filepath){
+
+		if( fs.statSync(filepath).isDirectory() ){
+			var metaPath = path.join(filepath, 'package.json');
+
+			if( fs.existsSync(metaPath) ){
+				modulePaths.push(filepath);
+			}
+			else{
+				modulePaths = modulePaths.concat(collectModulePaths(filepath));
+			}
+		}
+	});
+
+	return modulePaths;
+}
+
+function collectModules(filename){
+	return collectModulePaths(filename).map(function(modulePath){
+		var metaPath = path.join(modulePath, 'package.json');
+		var meta = JSON.parse(fs.readFileSync(metaPath));
+
 		return {
 			name: meta.name,
 			meta: meta,
@@ -48,6 +65,8 @@ function mergeDependencies(meta){
 	return names;
 }
 
+// we can have EPERM issues here: https://github.com/isaacs/rimraf/blob/master/rimraf.js
+// chmod 666 fix the problem I don't want to depend on rimraf
 function rmdirRecursive(filename){
 	var stat = fs.lstatSync(filename);
 
@@ -68,103 +87,128 @@ function rmdirRecursive(filename){
 
 var projectPath = process.cwd();
 var sourceFolder = path.resolve(projectPath, process.argv[2]);
+
+console.log('symlink will sync', sourceFolder, 'with', projectPath);
+
 var sourceModules = collectModules(sourceFolder);
 
 function findByName(array, name){
 	var i = 0, j = array.length;
 	for(;i<j;i++){
-		if( array[i].name === name ) return array[i]; 
+		if( array[i].name === name ) return array[i];
 	}
 	return null;
+}
+
+function symlinkModule(source, destination){
+	var lstat, exists = true;
+
+	try{
+		lstat = fs.lstatSync(destination);
+	}
+	catch(e){
+		if( e.code == 'ENOENT' ) exists = false;
+		else throw e;
+	}
+
+	if( exists ){
+		if( lstat.isSymbolicLink() ){
+			var linkPath = fs.readlinkSync(destination);
+
+			if( linkPath != source ){
+				console.log('remove previous link to', linkPath);
+				fs.unlinkSync(destination);
+			}
+			else{
+				console.log(destination, 'already linked to ', source);
+				return;
+			}
+		}
+		else{
+			console.log('rmdirRecursive', destination);
+			rmdirRecursive(destination);
+		}
+	}
+	// create a directory structure leading to the directory to put a symlink inside
+	else{
+		var directories = destination.split(path.sep), i = 0, j = directories.length -1, directory;
+		for(;i<j;i++){
+			directory = directories.slice(0, i + 1).join(path.sep);
+			if( !fs.existsSync(directory) ){
+				console.log('mkdir', directory);
+				fs.mkdirSync(directory);
+			}
+		}
+	}
+
+	console.log('linking', source, '->', destination);
+	fs.symlinkSync(source, destination, 'dir');
 }
 
 function symlink(projectPath){
 	var metaPath = projectPath + path.sep + 'package.json';
 	var nodeModulePath = projectPath + path.sep + 'node_modules';
-	
+
 	if( !fs.existsSync(metaPath) ){
 		throw new Error(projectPath + ' has no package.json');
 	}
 
-	var meta = JSON.parse(fs.readFileSync(metaPath));
-	var moduleNames = Object.keys(mergeDependencies(meta));
-	var usedSourceModules = sourceModules.filter(function(sourceModule){
-		var index = moduleNames.indexOf(sourceModule.name);
-		return index !== -1;
+	var toSymlink = [];
+
+	// always symlink sources into project
+	sourceModules.forEach(function(sourceModule){
+		var sourcePath = sourceModule.path;
+		var modulePath = path.join(nodeModulePath, sourceModule.name);
+		toSymlink.push({
+			source: sourcePath,
+			destination: modulePath
+		});
 	});
+
+	// symlink existing duplicate
+	if( fs.existsSync(nodeModulePath) ){
+		collectModules(projectPath).forEach(function(projectModule){
+			var sourceModule = findByName(sourceModules, projectModule.name);
+			if( sourceModule ){
+				if( sourceModule.meta.version === projectModule.meta.version ){
+					toSymlink.push({
+						source: sourceModule.path,
+						destination: projectModule.path
+					});
+				}
+				else{
+					console.warn(
+						'Cannot symlink: version mismatch',
+						'local', sourceModule.name, '@', sourceModule.meta.version, '!=',
+						'project', projectModule.name, '@', projectModule.meta.version
+					);
+				}
+			}
+		});
+	}
+
+	// do symlink, TODO: avoid symlink twice
+	toSymlink.forEach(function(symlinkArgs){
+		symlinkModule(symlinkArgs.source, symlinkArgs.destination);
+	});
+
 	var toInstall = [];
 
-	// add indirect dependency (dependency of dependency) recursively
-	function addIndirectDependency(sourceModule){
+	// check if any local modules has external dependency, if so they will be installed in the top folder
+	// not in the local modules itself (to prevent pollution of your source folder)
+	// if your local module has an external dependency version conflict it can be a problem
+	sourceModules.forEach(function(sourceModule){
 		Object.keys(sourceModule.dependencies).filter(function(dependency, index, dependencies){
-			// dependency already present
-			if( moduleNames.indexOf(dependency) !== -1 ) return;
-			moduleNames.push(dependency);
-
-			var indirectSourceModule = findByName(sourceModules, dependency);
-			if( indirectSourceModule ){
-				usedSourceModules.push(indirectSourceModule);
-				addIndirectDependency(indirectSourceModule);
-			}
-			else{
-				// if local sources of subdependency is not found, install them in the top folder
+			if( !findByName(sourceModules, dependency) ){
 				toInstall.push({
 					name: dependency,
 					value: sourceModule.dependencies[dependency]
 				});
 			}
 		});
-	}
-
-	usedSourceModules.forEach(addIndirectDependency);
-	
-	usedSourceModules.forEach(function(sourceModule){
-		var sourcePath = sourceModule.path;
-		var modulePath = path.join(nodeModulePath, sourceModule.name);
-		var lstat, exists = true;
-
-		try{
-			lstat = fs.lstatSync(modulePath);
-		}
-		catch(e){
-			if( e.code == 'ENOENT' ) exists = false;
-			else throw e;
-		}
-
-		if( exists ){
-			if( lstat.isSymbolicLink() ){
-				var linkPath = fs.readlinkSync(modulePath);
-
-				if( linkPath != sourcePath ){
-					console.log('remove previous link to', linkPath);
-					fs.unlinkSync(modulePath);
-				}
-				else{
-					console.log(modulePath, 'already linked to ', sourcePath);
-					return;
-				}				
-			}
-			else{
-				console.log('rmdirRecursive', modulePath);
-				rmdirRecursive(modulePath);
-			}
-		}
-		// create a directory structure leading to the directory to put a symlink inside
-		else{
-			var directories = modulePath.split(path.sep), i = 0, j = directories.length -1, directory;
-			for(;i<j;i++){
-				directory = directories.slice(0, i + 1).join(path.sep);
-				if( !fs.existsSync(directory) ){
-					console.log('mkdir', directory);
-					fs.mkdirSync(directory);
-				}
-			}
-		}
-
-		console.log('linking', sourcePath, '->', modulePath);
-		fs.symlinkSync(sourcePath, modulePath, 'dir');	
 	});
 
+	// do npm install, TODO: avoid npm install twice
 	toInstall.forEach(function(installArgs){
 		var name = installArgs.name;
 		var value = installArgs.value;
@@ -189,7 +233,7 @@ function symlink(projectPath){
 }
 
 if( process.env.npm_config_production !== 'true' ){
-	symlink(projectPath);	
+	symlink(projectPath);
 }
 
 process.exit();
